767d66c2f7dfad43b2071ea251f9dc622e8c3545
Pedro Henrique Oliveira da Silva <pedro.oliveira@extreme.digital>
2026-04-04 15:17:22 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   await usuarioService.criarAdminPadrao();
 
   app.enableCors({
-    origin: 'gac-web-beryl.vercel.app',
+    origin: ['http://localhost:4200', 'https://gac-web-beryl.vercel.app'],
     credentials: true,
   });
 
